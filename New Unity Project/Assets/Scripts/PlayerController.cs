b4b87540450f04dd4d6633a343d74945759ab2c4
Wallace Costa <wcosta@unifesp.br>
2025-01-25 23:31:22 -0300

    Fundo e tamanho do personagem

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _playerRidigibody2D;
+    public float _playerSpeed;
+    private Vector2 _playerDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
